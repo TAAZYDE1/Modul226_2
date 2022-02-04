8ee3d9a08600cfb94464649a45dfacf6706e0eb4
TAAZYDE1 <denis.zysset@swisscom.com>
2022-02-04 09:29:46 +0100

    Added auf2

---
+++ src/main/java/ab01/Auf02.java
@@ -0,0 +1,18 @@
+package ab01;
+
+public class Auf02 {
+    public static void main(String[] args){
+        public class Mitarbeiter{
+
+        }
+        public class Fachangestellte{
+
+        }
+        public class Chef{
+
+        }
+        public class Lernender{
+
+        }
+    }
+}
